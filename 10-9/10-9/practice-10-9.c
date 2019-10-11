62f88990d2be7c133c80160ba4c8627c012cc5f7
DoubleEar <1638435068@qq.com>
2019-10-12 00:15:50 +0800

    增加代码

--- 10-9/10-9/practice-10-9.c
+++ 10-9/10-9/practice-10-9.c
@@ -1,23 +1,28 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
-//写一个递归函数DigitSum(n)，输入一个非负整数，返回组成它的数字之和。
 
 #include<stdio.h>
-//void DigitSum(int x)
+//写一个递归函数DigitSum(n)，输入一个非负整数，返回组成它的数字之和。
+//int DigitSum(int n)
 //{
-//	if (x > 9)
+//	int ret = 0;
+//	int i = 0;
+//	if (n != 0)
 //	{
-//		DigitSum(x/10);
+//		i = n % 10;
+//		n = n / 10;
+//		ret = i + DigitSum(n);
 //	}
-//	printf("%d \n", x % 10);
+//	return ret;
 //}
-//
 //int main()
 //{
-//	int input = 0;
+//	int n = 0;
 //	printf("请输入一个非负整数：\n");
-//	scanf("%d", &input);
-//	DigitSum(input);
+//	scanf("%d", &n);
+//	int ret = DigitSum(n);
+//	printf("组成它的数字之和是:%d\n", ret);
 //	return 0;
+//
 //}
 
 //递归实现strlen
@@ -27,40 +32,81 @@
 //	{
 //		return 0;
 //	}
-//	else 
+//	else
 //		return  1 + my_strlen(str + 1);
 //}
 //
 //int main()
 //{
-//	char *p="0";
+//	char p[15];
 //	printf("请输入一个字符串:\n");
-//	scanf("%s", &p);
-//	int len=my_strlen(p);
+//	scanf("%s", p);    //字符串的输入不用&，直接用数组名
+//	int len = my_strlen(p);
 //	printf("%d\n", len);
+//	return 0;
+//}
+
+//循环实现strlen
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//
+//int main()
+//{
+//	char p[15];
+//	printf("请输入一个字符串:\n");
+//	scanf("%s", p);    //字符串的输入不用&，直接用数组名
+//	int len = my_strlen(p);
+//	printf("%d\n", len);
+//	return 0;
+//}
+
+// 用递归的方法编写一个函数 reverse_string(char * string)实现：将参数字符串中的字符反向排列。
+//void reverse_string(char * string)
+//{
+//	if (*(++string) != '\0')
+//	{
+//		reverse_string(string);
+//	}
+//	printf("%c", *(string - 1));
+//}
+//
+//int main()
+//{
+//	char p[15];
+//	printf("请输入一个字符串:\n");
+//	scanf("%s", p); //字符串的输入不用&，直接用数组名
+//	char n = reverse_str(p);
+//	printf("%s\n", n);
 //	return 0;
 //}
 
 
 //递归实现求n的阶乘 
-
-int fan(int n)
-{
-	if (n <= 1)
-		return 0;
-	else
-		return n*fan(n - 1);
-}
-
-int main()
-{
-	int n = 0;
-	int ret = 0;
-	scanf("%d\n", &n);
-	ret = fan(n);
-	printf("%d\n", ret);
-	return 0;
-}
+//int fan(int n)
+//{
+//	if (n == 1 || n == 0)
+//		return 1;
+//    else 
+//		return n*fan(n - 1);
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf("%d", &n);
+//	ret = fan(n);
+//	printf("%d\n", ret);
+//	return 0;
+//}
 
 //循环实现求n的阶乘 
 //int main()
@@ -104,7 +150,6 @@ int main()
 
 
 //用递归求斐波那契数列的第n项
-
 //int Fibonacci(int n){
 //	if (n == 1 || n == 2) {
 //		return 1;
@@ -119,3 +164,49 @@ int main()
 //	printf("%d\n", Fibonacci(n));
 //}
 //
+
+//递归方式实现打印一个整数的每一位
+//void print(int x)
+//{
+//	if (x > 9)
+//	{
+//	 print(x/10);
+//	}
+//	printf("%d \n", x % 10);	
+//}
+//
+//int main()
+//{
+//	int input = 0;
+//	printf("请输入一个非负整数：\n");
+//	scanf("%d", &input);
+//	print(input);
+//	return 0;
+//}
+
+//编写一个函数实现n^k，使用递归实现。
+//int mi(int x, int y)
+//{
+//	if (y <= 0)
+//		return 0;
+//	else if (x == 1)
+//		return 1;
+//	else if (y == 1)
+//	{
+//		return n;
+//	}
+//	else
+//	{
+//		return n * mi(x, y - 1);
+//	}
+//
+//
+//}
+//int main()
+//{
+//	int n = 0;
+//	int k = 0;
+//	scanf("%d%d", n, k)
+//	printf("%d\n",mi(n, k));
+//	return 0;
+//}
